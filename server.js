07b39b084c97b99d98e7bfb55fec85e49e097425
tarun <madhavtarun.0205@gmail.com>
2025-07-20 16:39:38 +0530

    Cors updated

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ import invoiceRoutes from './routes/invoice.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }));
+app.use(cors());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
